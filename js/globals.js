06026c6d289323d93576bfdb986486dc1e40a85e
brianpaulandaya <brianandaya@ymail.com>
2015-03-30 16:11:14 +0800

    Update globals.js

--- js/globals.js
+++ js/globals.js
@@ -14,13 +14,13 @@ define(["lib/knockout"], function (ko) {
     // If any of these things are supposed to change at any point, they should be stored in gameAttributes rather than here.
     g.confs = [{cid: 0, name: "Eastern Conference"}, {cid: 1, name: "Western Conference"}];
     g.divs = [{did: 0, cid: 0, name: "Atlantic"}, {did: 1, cid: 0, name: "Central"}, {did: 2, cid: 0, name: "Southeast"}, {did: 3, cid: 1, name: "Southwest"}, {did: 4, cid: 1, name: "Northwest"}, {did: 5, cid: 1, name: "Pacific"}];
-    g.salaryCap = 60000;  // [thousands of dollars]
-    g.minPayroll = 40000;  // [thousands of dollars]
-    g.luxuryPayroll = 65000;  // [thousands of dollars]
+    g.salaryCap = 63000;  // [thousands of dollars]
+    g.minPayroll = 56000;  // [thousands of dollars]
+    g.luxuryPayroll = 68000;  // [thousands of dollars]
     g.luxuryTax = 1.5;
     g.minContract = 500;  // [thousands of dollars]
     g.maxContract = 20000;  // [thousands of dollars]
-    g.minRosterSize = 10;
+    g.minRosterSize = 12;
 
     // Constants in all caps
     g.PHASE = {
